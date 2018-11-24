5fd9aca5ef1a2794fe9c2b656c4235f06d549568
5tarlight <developerstarlight@gmail.com>
2018-11-25 00:54:01 +0900

    brief access
    make useful variables in brief case

--- app.js
+++ app.js
@@ -19,26 +19,26 @@ client.on('message', message => {
   var author = message.author
   var content = message.content
 
-  if (message.content.startsWith(prefix + '핑')) {
+  if (content.startsWith(prefix + '핑')) {
     channel.send(embed.ping(client))
     logger.info(author.id + ' : ping')
-  } else if (message.content.startsWith(prefix + '업타임')) {
-    message.channel.send(embed.uptime(client))
-    logger.info(message.author.id + ' : uptime')
-  } else if (message.content.startsWith(prefix + '도움')) {
-    message.channel.send(embed.help(client, prefix))
-  } else if (message.content == (prefix + '재시작')){
-    if (message.author.id == '352755226224361482') {
+  } else if (content.startsWith(prefix + '업타임')) {
+    channel.send(embed.uptime(client))
+    logger.info(author.id + ' : uptime')
+  } else if (content.startsWith(prefix + '도움')) {
+    channel.send(embed.help(client, prefix))
+  } else if (content == (prefix + '재시작')){
+    if (author.id == '352755226224361482') {
       logger.debug('restarting...')
       logger.info('restarting...\n\n')
       process.exit(0)
     } else {
       message.reply('명령어 접근이 거부되었습니다.')
-      logger.debug(message.author.id + ' : restart denied')
-      logger.info(message.author.id + ' : restart denied')
+      logger.debug(author.id + ' : restart denied')
+      logger.info(author.id + ' : restart denied')
     }
   } else if (message.content.startsWith(`${prefix}기능도움`)) {
-    message.channel.send(embed.help_func(client, prefix))
+    channel.send(embed.help_func(client, prefix))
   }
 });
 
